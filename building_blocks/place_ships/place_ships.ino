58c32535b3db749fb3b48e23ffbced0942ce2933
ryanscofield03 <ryanscofield68@gmail.com>
2025-08-26 01:08:53 +1200

    worked on making our state machine for handling each state of the game - seems to be working from quickly testing with LEDs.
    Next will work on implementing draw, handleRotate, and handleAction for the place ship state

--- building_blocks/place_ships/place_ships.ino
+++ building_blocks/place_ships/place_ships.ino
@@ -101,8 +101,6 @@ bool ships[4][4][3][3] = {
     }
 };
 
-Adafruit_NeoPixel strip(NUMPIXELS, LED_MATRIX_PIN, NEO_GRB + NEO_KHZ800);
-
 enum Direction {
     LEFT, RIGHT, UP, DOWN
 };
@@ -148,40 +146,176 @@ struct BoardPosition {
 };
 
 enum class StateType {
-    StartState, PairingState, PlacingShipsState, WaitingOtherPlayerState, ShootingShipsState, EndState
-}
-
-struct PlacingShipsState {
-    private:
-        BoardPosition position;
-        BoardPosition shipPlacements[4];
-        int shipIndex;
-        int shipRotation;
+    Start,
+    PlaceShips,
+    ShootShips,
+    End
 };
 
-struct ShootingShipsState {
+struct GameContext {
+    bool canTransitionFlag = false;
+    BoardPosition position;
+    BoardPosition shipPlacements[4];
+    Adafruit_NeoPixel& screen;
+
+    GameContext(Adafruit_NeoPixel& screen) : screen(screen) {}
+};
+
+class GameState {
+    protected:
+        GameContext& context;
+    public:
+        GameState(GameContext& context) : context(context) {}
+        virtual ~GameState() {}
+        virtual void Draw() = 0;
+
+        virtual void HandleLeft() {
+            context.position.move(LEFT);
+        };
+
+        virtual void HandleRight() {
+            context.position.move(RIGHT);
+        };
+
+        virtual void HandleUp() {
+            context.position.move(UP);
+        };
+
+        virtual void HandleDown() {
+            context.position.move(DOWN);
+        };
+
+        virtual void HandleRotate() = 0;
+        virtual void HandleAction() = 0;
+        virtual StateType Type() = 0;
+};
+
+class StartGameState: public GameState {
+    public:
+        StartGameState(GameContext& context) : GameState(context) {}
+
+        void Draw() override {
+            Serial.println("Drawing StartGameState");
+            for (int i = 0; i < NUMPIXELS; i++) {
+                context.screen.setPixelColor(i, context.screen.Color(255, 19, 159));
+            }
+            context.screen.show();
+        }
+
+        void HandleRotate() override {
+            ;
+        }
+
+        void HandleAction() override {
+            context.canTransitionFlag = true;
+        }
+
+        StateType Type() override {
+            return StateType::Start;
+        }
+};
+
+class PlacingShipsState: public GameState {
+    public:
+        PlacingShipsState(GameContext& context) : GameState(context) {}
+
+        void Draw() override {
+            Serial.println("Drawing PlacingShipsState");
+            for (int i = 0; i < NUMPIXELS; i++) {
+                context.screen.setPixelColor(i, context.screen.Color(50, 100, 150));
+            }
+            context.screen.show();
+        }
+
+        void HandleRotate() override {
+            ;
+        }
+
+        void HandleAction() override {
+            ;
+        }
+
+        StateType Type() override {
+            return StateType::PlaceShips;
+        }
+};
+
+class ShootingShipsState: public GameState {
     private:
-        BoardPosition position;
-        BoardPosition shipPlacements[4];
         BoardPosition hitPlacements[64];
         bool myTurn;
         int shootSize;
 };
 
-struct Game {
+class Game {
     private:
-        StateType stateType;
+        GameState* currentState;
+        GameContext& context;
 
     public:
-        void nextState() {
-            if (stateType == StateType::StartState) {
-                stateType = StateType::PairingState;
+        Game(GameContext& context) : currentState(nullptr), context(context) {}
 
+        void SetState(GameState* newState) {
+            if (currentState) delete currentState;
+            currentState = newState;
+        }
+
+        void Update() {
+            bool leftPressed = digitalRead(LEFT_PIN) == LOW;
+            int upPressed = digitalRead(UP_PIN) == LOW;
+            int downPressed = digitalRead(DOWN_PIN) == LOW;
+            int rightPressed = digitalRead(RIGHT_PIN) == LOW;
+            int rotatePressed = digitalRead(ROTATE_PIN) == LOW;
+            int actionPressed = digitalRead(ACTION_PIN) == LOW;
+
+            if (leftPressed) {
+                Serial.println("Left pressed");
+                delay(250);
+                currentState->HandleLeft();
+            } else if (upPressed) {
+                Serial.println("Up pressed");
+                delay(250);
+                currentState->HandleUp();
+            } else if (downPressed) {
+                Serial.println("Down pressed");
+                delay(250);
+                currentState->HandleDown();
+            } else if (rightPressed) {
+                Serial.println("Right pressed");
+                delay(250);
+                currentState->HandleRight();
+            } else if (rotatePressed) {
+                Serial.println("Rotate pressed");
+                delay(250);
+                currentState->HandleRotate();
+            } else if (actionPressed) {
+                Serial.println("Action pressed");
+                delay(250);
+                currentState->HandleAction();
             }
+
+            currentState->Draw();
+
+            if (context.canTransitionFlag) {
+                switch (currentState->Type()) {
+                    case StateType::Start:
+                        SetState(new PlacingShipsState(context));
+                        context.canTransitionFlag = false;
+                        break;
+                }
+            }
+
+            delay(250);
+        }
+
+        ~Game() {
+            delete currentState;
         }
 };
 
-BoardPosition playerPosition;
+Adafruit_NeoPixel screen(NUMPIXELS, LED_MATRIX_PIN, NEO_GRB + NEO_KHZ800);
+GameContext context(screen);
+Game game(context);
 
 void setup() {
     pinMode(LEFT_PIN, INPUT_PULLUP);
@@ -191,62 +325,18 @@ void setup() {
     pinMode(ROTATE_PIN, INPUT_PULLUP);
     pinMode(ACTION_PIN, INPUT_PULLUP);
 
-    strip.begin();
-    strip.setBrightness(2);
-    strip.show();
+    screen.begin();
+    screen.setBrightness(2);
+    screen.show();
 
     Serial.begin(9600);
     delay(1000);
+
+    game.SetState(new StartGameState(context));
 }
 
 
 void loop() {
-    bool leftPressed = digitalRead(LEFT_PIN) == LOW;
-    int upPressed = digitalRead(UP_PIN) == LOW;
-    int downPressed = digitalRead(DOWN_PIN) == LOW;
-    int rightPressed = digitalRead(RIGHT_PIN) == LOW;
-    int rotatePressed = digitalRead(ROTATE_PIN) == LOW;
-    int actionPressed = digitalRead(ACTION_PIN) == LOW;
-
-    if (leftPressed) {
-        Serial.println("Left pressed");
-        playerPosition.move(LEFT);
-        playerPosition.print();
-        delay(250);
-    } else if (upPressed) {
-        Serial.println("Up pressed");
-        playerPosition.move(UP);
-        playerPosition.print();
-        delay(250);
-    } else if (downPressed) {
-        Serial.println("Down pressed");
-        playerPosition.move(DOWN);
-        playerPosition.print();
-        delay(250);
-    } else if (rightPressed) {
-        Serial.println("Right pressed");
-        playerPosition.move(RIGHT);
-        playerPosition.print();
-        delay(250);
-    } else if (rotatePressed) {
-        Serial.println("Rotate pressed");
-        delay(250);
-    } else if (actionPressed) {
-        Serial.println("Action pressed");
-        delay(250);
-    }
-
-    // TODO: we will have a 1 second delay when redrawing the board currently
-    // - need to delay after drawing but only if any button was pressed
-
-    int playerIndex = playerPosition.to_index();
-    for (int i = 0; i < NUMPIXELS; i++) {
-        if (i == playerIndex) {
-            strip.setPixelColor(i, strip.Color(255, 19, 159));
-        } else {
-            strip.setPixelColor(i, strip.Color(0, 0, 0));
-        }
-    }
-    strip.show();
+    game.Update();
     delay(100);
 }
